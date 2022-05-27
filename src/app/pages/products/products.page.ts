d9797a019fb2bd937ba2553f96749413292460aa
Vu Nguyen <phihoangvu.nguyen@evizi.com>
2022-05-27 17:13:57 +0700

    Add Product Item component

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,5 +1,7 @@
+/* eslint-disable quote-props */
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/services';
 import { ProductService } from 'src/app/services/product.service';
 import { WoocommerceProductsService } from 'src/app/services/products/woocommerce-products.service';
 @Component({
@@ -8,16 +10,29 @@ import { WoocommerceProductsService } from 'src/app/services/products/woocommerc
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-  data : any
+  data: Product[];
+  rowData: any[] = [];
   constructor(public productService: ProductService,
     private woocommerceProductsService: WoocommerceProductsService,
     private httpClient: HttpClient
     ) { }
 
   ngOnInit() {
+    this.prepareData();
+  }
+
+  prepareData() {
     this.woocommerceProductsService.getAllProducts().subscribe(result => {
       if (result) {
-        this.data = JSON.stringify(result);
+        this.data = result;
+        while( result.length > 0) {
+          const chuck = result.splice(0, 3);
+          this.rowData.push([
+          chuck[0],
+          chuck[1] || {},
+          chuck[2] || {}
+          ]);
+        }
       }
     });
   }
