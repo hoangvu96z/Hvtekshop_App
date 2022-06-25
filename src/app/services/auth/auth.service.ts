33a48c1763553a1f9fafb3b950ee9d4de51271bf
VU_PC_01\hoang <hoangvu96z@gmail.com>
2022-06-25 13:56:18 +0700

    Add login page

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { WoocommerceHelperService } from '../helper.service';
 import { CreateNonce, CreateNonceRes, RegisterPayload, LoginPayload } from './auth.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 // Plugins used https://wordpress.org/plugins/json-api-user/
 
@@ -12,11 +12,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<any>;
+  private currentUser: Observable<any>;
 
   constructor(
     private httpClient: HttpClient,
     private wooHelper: WoocommerceHelperService
-  ) { }
+  ) {
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   createNonce(payload: CreateNonce): Observable<CreateNonceRes> {
     return this.httpClient.get(`api/get_nonce/`, {params: this.wooHelper.includeQuery(payload)})
@@ -43,5 +48,26 @@ export class AuthService {
   generateAuthCookie(data: LoginPayload): Observable<any> {
     return this.httpClient.post(`api/user/generate_auth_cookie/`, this.wooHelper.includeEncoded(data))
       .pipe(catchError(err => this.wooHelper.handleError(err)));
+  }
+
+  login(username, password) {
+      return this.httpClient.post<any>(`api/user/authenticate`, { username, password })
+          .pipe(map(user => {
+              // store user details and jwt token in local storage to keep user logged in between page refreshes
+              localStorage.setItem('currentUser', JSON.stringify(user));
+              this.currentUserSubject.next(user);
+              return user;
+          }));
+  }
+
+  logout() {
+      // remove user from local storage and set current user to null
+      localStorage.removeItem('currentUser');
+      this.currentUserSubject.next(null);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
   }
 }
